addf6d50c07e62086bdf47b43dbdc651a7705947
romkravets <romann.kravets@gmail.com>
2019-02-26 22:10:49 +0200

    meme

---
+++ docs/precache-manifest.4d9225484c5cd583644d6030a46aacb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/static/media/impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8393885f60e564ff281",
+    "url": "/static/js/main.e8393885.chunk.js"
+  },
+  {
+    "revision": "fb20b090c888caf4c959",
+    "url": "/static/js/1.fb20b090.chunk.js"
+  },
+  {
+    "revision": "e8393885f60e564ff281",
+    "url": "/static/css/main.fe359feb.chunk.css"
+  },
+  {
+    "revision": "fb20b090c888caf4c959",
+    "url": "/static/css/1.b2cd75d5.chunk.css"
+  },
+  {
+    "revision": "5e35bcc757d9c9ba5445a9f24bc12252",
+    "url": "/index.html"
+  }
+];
